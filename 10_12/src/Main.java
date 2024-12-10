cd1b51d59c1bb55ce0256fea3fb18814768c1e9d
Qor1 <ivans.volosovs@gmail.com>
2024-12-10 09:31:57 +0200

    Add files via upload

---
+++ 10_12/src/Main.java
@@ -0,0 +1,36 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+
+    //       1	Make the cookie batter.
+    public static void better() {
+        System.out.println("Mix the dry ingredients.");
+        System.out.println("Cream the butter and sugar.");
+        System.out.println("Beat in the eggs.");
+        System.out.println("Stir in the dry ingredients.");
+//        2	Bake the cookies.
+    }
+
+    public static void better1() {
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+        System.out.println("Set the oven temperature.");
+        System.out.println("Set the timer.");
+        System.out.println("Place a batch of cookies into the oven.");
+        System.out.println("Allow the cookies to bake.");
+//        3	Add frosting and sprinkles.
+    }
+
+    public static void better2() {
+        System.out.println("Mix ingredients for frosting.");
+        System.out.println("Spread frosting and sprinkles.");
+    }
+}
+
